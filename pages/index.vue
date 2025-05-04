e18080f9fe1ea71f333c22930564780c76e25962
Jose2580MEGA <josemeza23001@gmail.com>
2025-05-04 16:03:29 -0500

    paginasDelOva

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,25 @@
 <template>
     <h1>Objeto Virtual Aprendizaje 2025</h1>
     <aside>
-        <button>Inicio</button>
+        <button>
+            <nuxt-link to="index.vue">
+                Inicio
+            </nuxt-link>
+        </button>
+        <button>
+            <nuxt-link to="temas.vue">
+                Contenido
+            </nuxt-link>
+        </button>
+        <button>
+            <nuxt-link to="actividades.vue">
+                Actividades
+            </nuxt-link>
+        </button>
+        <button>
+            <nuxt-link to="examen.vue">
+                Examen
+            </nuxt-link>
+        </button>
     </aside>
 </template>
